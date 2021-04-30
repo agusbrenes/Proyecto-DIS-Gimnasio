01f18fbce9b3747a7bcc901d7283549eafa9f3e6
agusbrenes <56280607+agusbrenes@users.noreply.github.com>
2021-04-29 20:32:16 -0600

    Instructor added

--- Modelo/Instructor.js
+++ Modelo/Instructor.js
@@ -1,14 +1,12 @@
-import { Al_Dia, Moroso } from './status';
 import User from './user';
 
-export default class Client extends User {
+export default class Instructor extends User {
     constructor(email, password) {
         super(email, password);
         this.#id = null;
         this.#firstName = null;
         this.#lastName = null;
         this.#phone = null;
-        this.#status = null;
     }
 
     /**
@@ -76,17 +74,4 @@ export default class Client extends User {
     get getPhone() {
         return this.#phone;
     }   
-
-    set setAlDia() {
-        this.#status = Al_Dia;
-    }  
-
-    set setMoroso() {
-        this.#status = Moroso;
-    }  
-
-    get getStatus() {
-        return this.#status;
-    }
-
 }
---
+++ Modelo/TempInstructor.js
@@ -0,0 +1,2 @@
+import Instructor from './instructor';
+
