b4a1f799dcce31bd249726bb53db2423868e44f2
agusbrenes <56280607+agusbrenes@users.noreply.github.com>
2021-05-25 16:30:46 -0600

    Payment & Subscription (TODO)

---
+++ Modelo/Subscription.js
@@ -0,0 +1,43 @@
+export default class Subscription {
+    static #id = 0;
+
+    constructor(client) {
+        this.#id = Subscription.assignId();
+        this.#client = client;
+        this.#fee = 0;
+        this.#limit = 0;
+        this.#sessionCost = 1.0; // TODO conseguir precio de sesion
+    }
+
+    static assignId() {
+        return ++this.#id;
+    }
+
+    get getId() {
+        return this.#id;
+    }
+
+    get getClient() {
+        return this.#client;
+    }
+
+    get getFee() {
+        return this.#fee;
+    }
+
+    /**
+     * @param {float} sessionCost
+     */
+    set setSessionCost(sessionCost) {
+        this.#sessionCost = sessionCost;
+    }
+
+    get getSessionCost() {
+        return this.#sessionCost;
+    }
+
+    pay(amount) {
+        this.#fee += amount;
+        this.#limit = this.#fee / this.#sessionCost;
+    }
+}
