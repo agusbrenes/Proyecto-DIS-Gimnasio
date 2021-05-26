260ef56db8738ecc8fc6229a2a0f79542b0eca5d
agusbrenes <56280607+agusbrenes@users.noreply.github.com>
2021-05-25 19:52:28 -0600

    Client ready

--- Modelo/Client.js
+++ Modelo/Client.js
@@ -1,24 +1,28 @@
-import { Al_Dia, Moroso } from './status';
-import User from './user';
+const Status = require('./status');
+const User = require('./user');
 
-export default class Client extends User {
+module.exports = class Client extends User {
     constructor(email, password, id, firstName, lastName, phone) {
         super(email, password, id, firstName, lastName, phone);
         this.#status = Al_Dia;
         this.#reservations = new Map();
-        this.#subscriptions = [];
+        this.#subscriptions = new Map();
     }
 
-    set setAlDia() {
-        this.#status = Al_Dia;
+    setAlDia() {
+        this.#status = Status.Al_Dia;
     }  
 
-    set setMoroso() {
-        this.#status = Moroso;
+    setMoroso() {
+        this.#status = Status.Moroso;
     }  
 
     get getStatus() {
         return this.#status;
+    }
+
+    get getReservations() {
+        return this.#reservations;
     }
 
     get getReservation(id) {
@@ -37,6 +41,25 @@ export default class Client extends User {
             throw new Error("This reservation doesn't exist in the Session. Cannot perform delete operation.");
         }
         this.#reservations.delete(id);
+    }
+
+    get getSubscription() {
+        return this.#subscriptions;
+    }
+
+    get getSubscriptions(id) {
+        return this.#subscriptions.get(id);
+    }
+
+    addSubscription(subscription) {
+        this.#subscriptions.set(subscription.getId(), subscription);
+    }
+
+    deleteSubscription(id) {
+        if (this.#subscriptions.get(id) == undefined) {
+            throw new Error("This subscription doesn't exist in the User. Cannot perform delete operation.");
+        }
+        this.#subscriptions.delete(id);
     }
 
 }
