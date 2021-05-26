61c50644415aa720e64212274aee3ce55c7b62a6
Chuz23 <48294718+Chuz23@users.noreply.github.com>
2021-05-25 19:43:14 -0600

    Cambios de interfaz

--- interfaz/src/Windows/Register.js
+++ interfaz/src/Windows/Register.js
@@ -1,11 +1,75 @@
 import React, {Component} from "react";
 import axios from "axios";
+import "../App.css"
 
 class Register extends Component {
+    state = {
+        name: "",
+        lastname: "",
+        email: "",
+        id: "",
+        phone: "",
+        password: ""
+    }
+
+    //Función que actualiza los states
+    handleChange = (event) => {
+        const target = event.target;
+        const name = target.name;
+        const value = target.value;
+
+        this.setState({
+            [name] : value
+        });
+    }
+
+    submit = () => {
+        
+    }
+
     render() {
         return (
-            <div>
-                Register
+            <div className="Register">
+                <form onSubmit={this.submit}>
+                    <h4 className="text-center">
+                        Ingrese sus datos
+                    </h4>
+                    <div className="form-group">
+                        <label for="email">Correo Electronico</label>
+                        <input type="email" className="form-control" id="email" placeholder="nombre@ejemplo.com"/>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <label for="name">Nombre</label>
+                            <input type="text" className="form-control" id="name" placeholder="Nombre"/>
+                        </div>
+                        <div className="col">
+                            <label for="lastname">Apellido</label>
+                            <input type="text" className="form-control" id="lastname" placeholder="Apellido"/>
+                        </div>
+                    </div>
+                    <div className="form-group">
+                        <label for="id">Cédula</label>
+                        <input type="text" className="form-control" id="id" placeholder="#####"/>
+                    </div>
+                    <div className="form-group">
+                        <label for="phone">Número Telefono</label>
+                        <input type="text" className="form-control" id="phone" placeholder="#####"/>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <label for="password">Contraseña</label>
+                            <input type="password" className="form-control" id="password"/>
+                        </div>
+                        <div className="col">
+                            <label for="confirm">Confirmar Contraseña</label>
+                            <input type="password" className="form-control" id="confirm"/>
+                        </div>
+                    </div>
+                    <button type="submit" className="btn btn-primary" style={{marginTop:"20px"}}>
+                        Registrarse
+                    </button>
+                </form>
             </div>
         )
     }
