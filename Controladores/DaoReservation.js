cd6577fb4f82bc2e35197d97a49914d5bee3b0d4
Exotronic <ecotro2014gd@gmail.com>
2021-06-01 23:39:53 -0600

    Update DaoReservation.js

--- Controladores/DaoReservation.js
+++ Controladores/DaoReservation.js
@@ -12,7 +12,42 @@ const ReservationSchema = mongoose.model("Reservation", new Schema ({
         id: {type: Number}
     },
     paymentMethod: {
-        id: {type: Number}
+        id: {type: Number, default: undefined}
     },
     isConfirmed: {type: Boolean}
 }));
+
+module.exports = class DaoReservation extends Dao {
+    async find(filter) {
+        return await ReservationSchema.findOne(filter);
+    }
+
+    async save(object) {
+        const schema = this.toMongoSchema(object);
+        return await schema.save();
+    }
+
+    async delete(filter) {
+        return await ReservationSchema.remove(filter);
+    }
+
+    async modify(filter, object) {
+        return await ReservationSchema.updateOne(filter);
+    }
+
+    toMongoSchema(object) {
+        return new ReservationSchema({
+            id: object.id,
+            client: {
+                email: object.client.email
+            },
+            session: {
+                id: object.session.id
+            },
+            paymentMethod: {
+                id: object.paymentMethod.id
+            },
+            isConfirmed: object.isConfirmed
+        });
+    }
+}
