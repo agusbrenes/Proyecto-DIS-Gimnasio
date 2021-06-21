04507ae21b9d27466f8c254fe8c6e72d25369787
Exotronic <ecotro2014gd@gmail.com>
2021-06-20 21:55:12 -0600

    Refactor DAOs again (again)

--- Control/DAOs/DaoReservation.js
+++ Control/DAOs/DaoReservation.js
@@ -17,7 +17,13 @@ const ReservationSchema = mongoose.model("Reservation", new Schema ({
         },
         service: {
             name: {type: String}
-        }
+        },
+        schedule: {
+            initialHour: {type: Date, required: true},
+            totalHours: {type: Number},
+            month: {type: Number},
+            day: {type: Number}
+        },
     },
     paymentMethod: {
         name: {type: String}
@@ -55,6 +61,12 @@ module.exports = class DaoReservation extends Dao {
             },
             service: {
                 name: object.session.service.name
+            },
+            schedule: {
+                initialHour: object.session.schedule.initialHour,
+                totalHours: object.session.schedule.totalHours,
+                month: object.session.schedule.month,
+                day: object.session.schedule.day
             }
         };
         schema.paymentMethod = {
@@ -83,6 +95,12 @@ module.exports = class DaoReservation extends Dao {
                 },
                 service: {
                     name: object.session.service.name
+                },
+                schedule: {
+                    initialHour: object.session.schedule.initialHour,
+                    totalHours: object.session.schedule.totalHours,
+                    month: object.session.schedule.month,
+                    day: object.session.schedule.day
                 }
             },
             paymentMethod: {
